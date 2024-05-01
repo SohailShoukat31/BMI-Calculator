a27e375dc3173dd9a4e8fde170a119a301b6d05d
sohailahmed31 <160041648+sohailahmed31@users.noreply.github.com>
2024-05-01 15:00:50 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightInKg = 70;
+var heightInMeters = 1.75;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("My BMI is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKg = 70;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`My BMI is ${bmi}`);
